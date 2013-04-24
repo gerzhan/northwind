d676157ed2478fb4245d22983acd260b7fc62598
Kaustubh <kaustubh.malgaonkar@focalworks.in>
2013-04-24 18:21:08 +0530

    Customer Folder Created

--- NorthWind/assets/www/js/cust_module.js
+++ NorthWind/assets/www/js/cust_module.js
@@ -5,3 +5,22 @@ var cust_module = angular.module('cust_module', []);
 cust_module.config(['$httpProvider', function($httpProvider) {
     delete $httpProvider.defaults.headers.common["X-Requested-With"]
 }]);
+
+/*Sharing docto data*/
+cust_module.factory('Cust_details_list', ['$http', '$rootScope', function ($http, $rootScope) {
+	var doctors = [];
+	return {
+		getCustomerDetails: function($params) {
+			return $http({
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        url: 'http://localhost/northwind/backend/ajax/mobile',
+        method: "POST",
+        data: $params,
+      })
+        .success(function(addData) {
+          doctors = addData;
+          //$rootScope.$broadcast('handleSharedDoctorProfiles',doctors);
+        });
+		}
+	};
+}]);
