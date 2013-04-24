3674e5f3a9e2ab2e7477e8a6d2f1f0e81c732167
Amitav Roy <reachme@amitavroy.com>
2013-04-24 17:27:05 +0530

    Mistaken Clean plate to infinity

--- backend/application/helpers/global_helper.php
+++ backend/application/helpers/global_helper.php
@@ -1,38 +1,35 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-// getting the dsm function
 if (!function_exists('dsm'))
 {
-  function dsm($var) {
-    print '<pre clas="spit">';
+  /**
+   * This function prints the array in a pre formatter style.
+   * @param array $var
+   * @param bool $stop
+   */
+  function dsm($var, $stop = false) {
+    print '<pre class="spit">';
     print_r($var);
     print '</pre>';
+
+    if ($stop) {
+      exit();
+    }
   }
 }
 
-// getting the var_dump and die function
 if (!function_exists('dd'))
 {
-  function dd($var) {
-    var_dump($var);die();
-  }
-}
-
-// checking the user
-if (!function_exists('auth_user'))
-{
   /**
-   * This function will create a CI instance,
-   * check the session for auth key and validate.
-   * If failed, will redirect to login page.
+   * This function will var dump the array
+   * and exit
+   * @param $var
    */
-  function auth_user() {
-    // getting the CI instance
-    $ci =& get_instance();
-    $auth = $ci->session->userdata('auth');
-    if (!$auth) {
-      redirect('users/login');
-    }
+  function dd($var) {
+    print '<pre class="spit">';
+    var_dump($var);
+    print '</pre>';
+    exit();
   }
 }
 
@@ -41,12 +38,51 @@ if (!function_exists('check_if_post'))
   /**
    * This function will check if the page has POST data
    * and return true or false accordingly.
+   * @param null $redirect_path
    * @return bool
    */
-  function check_if_post() {
+  function check_if_post($redirect_path = null) {
     if (!empty($_POST) && is_array($_POST))
       return true;
     else
-      return false;
+      redirect($redirect_path,'',302);
+  }
+}
+
+/**
+ * This function will check for the user's session and determine
+ * if the user is authenticated or not.
+ */
+if (!function_exists('auth_user'))
+{
+  function auth_user() {
+    return TRUE;
+  }
+}
+
+if (!function_exists('set_message'))
+{
+  /**
+   * This message will set a session variable message
+   * which will be used to pass messages and alerts.
+   * @param $message
+   * @param string $type
+   * @return bool
+   */
+  function set_message($message, $type = 'info') {
+    return true;
+  }
+}
+
+if (!function_exists('get_message'))
+{
+  /**
+   * This message will be used to get any message
+   * which might have been set by some code in the session
+   * and then display the message
+   * @return bool
+   */
+  function get_message() {
+    return true;
   }
 }
