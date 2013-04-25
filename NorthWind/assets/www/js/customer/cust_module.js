7f322b8d399b5949762dcf41722337295a397474
Amitav Roy <reachme@amitavroy.com>
2013-04-25 18:37:51 +0530

    Route service added so now product display is done using route service and data is being displayed as per the category.

--- NorthWind/assets/www/js/customer/cust_module.js
+++ NorthWind/assets/www/js/customer/cust_module.js
@@ -1,6 +1,10 @@
 /*Define module*/
 var serverName = 'http://192.168.3.76/northwind/backend/';
+
 var getCustomersListURL = serverName + 'ajax/mobile';
+var getCategoryListURL = serverName + 'ajax/mobile/get_categories';
+var getProductListURL = serverName + 'ajax/mobile/product_by_id/';
+
 var saveCustomerDataURL = serverName + 'ajax/mobile/save_customer_profile'
 
 var customerModule = angular.module('customerModule', []);
@@ -10,15 +14,38 @@ customerModule.config(['$httpProvider', function($httpProvider) {
 	delete $httpProvider.defaults.headers.common["X-Requested-With"]
 }]);
 
+/*Routes are defined here*/
+customerModule.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/categories', {templateUrl: 'categories.html', controller: customerModule.customerList});
+  $routeProvider.when('/products/:categoryId', {templateUrl: 'products.html', controller: customerModule.productList});
+  $routeProvider.otherwise({redirectTo: '/categories'});
+}]);
+
 /*Sharing docto data*/
 customerModule.factory('CustDetailsList', ['$http', '$rootScope', function ($http, $rootScope) {
 	var customers = [];
+	var categories = [];
+	var products = [];
 	return {
 		getCustomersList: function() {
 			return $http.get(getCustomersListURL).then(function(response) {
 				customers = response.data;
 				$rootScope.$broadcast('getCustomersList',customers);
 				return customers;
+			})
+		},
+		getCategoryList: function() {
+			return $http.get(getCategoryListURL).then(function(response) {
+				categories = response.data;
+				$rootScope.$broadcast('getCategoryList',categories);
+				return categories;
+			})
+		},
+		getProductsList: function(categoryId) {
+			return $http.get(getProductListURL + categoryId).then(function(response) {
+				products = response.data;
+				$rootScope.$broadcast('getProductsList',products);
+				return products;
 			})
 		},
 		saveCustomerData: function($params) {
--- NorthWind/assets/www/js/customer/customer_app.js
+++ NorthWind/assets/www/js/customer/customer_app.js
@@ -13,13 +13,23 @@ customerModule.controller('customerList', function($scope,CustDetailsList){
 
 	$scope.postData =function(customerdata){
 		$params = $.param({'data' : JSON.stringify(customerdata)});
-
 		CustDetailsList.saveCustomerData($params);
-		
-
 		$scope.$on('getCustomersList', function(events, customers) {
       $scope.customers = customers;
 		});
 
 	}
+});
+
+customerModule.controller('productsAndCategories', function($scope, CustDetailsList) {
+	CustDetailsList.getCategoryList().then(function(categories) {
+		$scope.categories = categories;
+	});
+});
+
+customerModule.controller('productList', function($scope, CustDetailsList, $routeParams) {
+	$scope.categoryID = $routeParams.categoryId;
+	CustDetailsList.getProductsList($scope.categoryID).then(function(products) {
+		$scope.products = products;
+	});
 });
